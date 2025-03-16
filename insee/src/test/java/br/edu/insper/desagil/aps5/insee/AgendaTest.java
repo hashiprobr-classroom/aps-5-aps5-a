c04a3586ed163b8fa5b0672c5bede6d9a3a29407
eduardotomiya <eduardodt@al.insper.edu.br>
2025-03-16 09:53:18 -0300

    agenda test incompleto

--- insee/src/test/java/br/edu/insper/desagil/aps5/insee/AgendaTest.java
+++ insee/src/test/java/br/edu/insper/desagil/aps5/insee/AgendaTest.java
@@ -1,4 +1,35 @@
 package br.edu.insper.desagil.aps5.insee;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+
 public class AgendaTest {
+
+    private Agenda agenda = new Agenda();
+
+    @Test
+    public void constroi() {
+        assertTrue(agenda.getEventos().isEmpty());
+    }
+
+    @Test
+    public void adicionaValido() {
+        // nao soube fazer
+    }
+
+    @Test
+    public void adicionaInvalido() {
+        DataTempo inicio = new DataTempo(new Data(), new Tempo());
+        DataTempo fim = new DataTempo(new Data(), new Tempo());
+        Evento evento = new Evento(inicio, fim);
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> agenda.adiciona(evento));
+
+        assertEquals("Evento inválido!", e.getMessage());
+        assertTrue(agenda.getEventos().isEmpty());
+    }
 }
--- insee/src/test/java/br/edu/insper/desagil/aps5/insee/TempoTest.java
+++ insee/src/test/java/br/edu/insper/desagil/aps5/insee/TempoTest.java
@@ -3,49 +3,45 @@ package br.edu.insper.desagil.aps5.insee;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-class TempoTest {
+public class TempoTest {
 
     Tempo tempo = new Tempo();
 
     @Test
-    void constroi() {
-
+    public void constroi() {
         assertEquals(0, tempo.getHora());
         assertEquals(0, tempo.getMinuto());
     }
 
     @Test
-    void naoAtualizaMenor() {
-
+    public void naoAtualizaMenor() {
         tempo.atualiza(-1, -1);
         assertEquals(0, tempo.getHora());
         assertEquals(0, tempo.getMinuto());
     }
 
     @Test
-    void atualizaMaior() {
-
+    public void atualizaMaior() {
         tempo.atualiza(24, 60);
         assertEquals(23, tempo.getHora());
         assertEquals(59, tempo.getMinuto());
     }
 
     @Test
-    void atualiza() {
-
+    public void atualiza() {
         tempo.atualiza(15, 30);
         assertEquals(15, tempo.getHora());
         assertEquals(30, tempo.getMinuto());
     }
 
     @Test
-    void comoZero() {
+    public void comoZero() {
 
         assertEquals(0, tempo.comoInteiro());
     }
 
     @Test
-    void comoAgora() {
+    public void comoAgora() {
 
         tempo.atualiza(15, 30);
         assertEquals(930, tempo.comoInteiro());
